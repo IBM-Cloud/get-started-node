043ee1da1a8c1d301f07294a7f6aedb4436224db
Ross Fenrick <rossfenrick@us.ibm.com>
2016-07-07 10:18:26 -0500

    added cfignore and updated tab spacing in server.js

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-    var main = require("./lib/main");
-    var _ = require("underscore");
-    var express = require("express");
-    var app = express();
-    var port = 8080;
-    var options;
-    var shortOptions;
-    var envOptions = {};
-    app.use(express.static(__dirname + '/views'));
+var main = require("./lib/main");
+var _ = require("underscore");
+var express = require("express");
+var app = express();
+var port = 8080;
+var options;
+var shortOptions;
+var envOptions = {};
+app.use(express.static(__dirname + '/views'));
 
 
-    // Guide Part 1 - loading the To-Do without Cloudant.
-    app.listen(port, function() {
-        console.log("To view your app, open this link in your browser: http://localhost:" + port);
+// Guide Part 1 - loading the To-Do without Cloudant.
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
+
+
+
+
+
+// Guide Part 2 - Using Cloudant
+/*
+ if (process.env.PORT != null) {
+        envOptions.port = process.env.PORT;
+    }
+    options = _.defaults(envOptions, {
+        db: 'cloudant'
     });
-
-
-
-
-
-    // Guide Part 2 - Using Cloudant
-    /*
-     if (process.env.PORT != null) {
-            envOptions.port = process.env.PORT;
-        }
-        options = _.defaults(envOptions, {
-            db: 'cloudant'
-        });
-        return main.start(options);
-     */
+    return main.start(options);
+ */
