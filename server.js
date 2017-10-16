7885437db9d1a7bfb8027b55576685a1bf8b9108
Michael Peters <opiethehokie@gmail.com>
2017-10-16 17:21:11 -0400

    adds support for user-provided cloudant service (#14)

--- server.js
+++ server.js
@@ -74,12 +74,18 @@ const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
 
 const appEnv = cfenv.getAppEnv(appEnvOpts);
 
-if (appEnv.services['cloudantNoSQLDB']) {
+if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
   // Load the Cloudant library.
   var Cloudant = require('cloudant');
 
   // Initialize database with credentials
-  var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+  if (appEnv.services['cloudantNoSQLDB']) {
+     // CF service named 'cloudantNoSQLDB'
+     var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+  } else {
+     // user-provided service with 'cloudant' in its name
+     var cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);
+  }
 
   //database name
   var dbName = 'mydb';
