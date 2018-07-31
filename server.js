531373480cd3e07e66855cacbd6a66d61cf9be9b
Andrew Rieder <Andrew.Rieder@ibm.com>
2018-07-31 09:02:00 -0400

    Added support for MongoDB.

--- server.js
+++ server.js
@@ -9,12 +9,76 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-var mydb;
+let mydb;
+var vendor; // Because the MongoDB and Cloudant use different API commands, we
+            // have to check which command should be used based on the database
+            // vendor.
+let dbName = 'mydb';
+
+// Separate functions are provided for inserting/retrieving content from
+// MongoDB and Cloudant databases. These functions must be prefixed by a
+// value that may be assigned to the 'vendor' variable, such as 'mongodb' or
+// 'cloudant' (i.e., 'cloudantInsertOne' and 'mongodbInsertOne')
+
+var insertOne = {};
+var getAll = {};
+
+insertOne.cloudant = function(doc, response) {
+  mydb.insert(doc, function(err, body, header) {
+    if (err) {
+      console.log('[mydb.insert] ', err.message);
+      response.send("Error");
+      return;
+    }
+    doc._id = body.id;
+    response.send(doc);
+  });
+}
+
+getAll.cloudant = function(response) {
+  var names = [];  
+  mydb.list({ include_docs: true }, function(err, body) {
+    if (!err) {
+      body.rows.forEach(function(row) {
+        if(row.doc.name)
+          names.push(row.doc.name);
+      });
+      response.json(names);
+    }
+  });
+  //return names;
+}
+
+let collectionName = 'mycollection'; // MongoDB requires a collection name.
+
+insertOne.mongodb = function(doc, response) {
+  mydb.collection(collectionName).insertOne(doc, function(err, body, header) {
+    if (err) {
+      console.log('[mydb.insertOne] ', err.message);
+      response.send("Error");
+      return;
+    }
+    doc._id = body.id;
+    response.send(doc);
+  });
+}
+
+getAll.mongodb = function(response) {
+  var names = [];
+  mydb.collection(collectionName).find({}, {fields:{_id: 0, count: 0}}).toArray(function(err, result) {
+    if (!err) {
+      result.forEach(function(row) {
+        names.push(row.name);
+      });
+      response.json(names);
+    }
+  });
+}
 
 /* Endpoint to greet and add a new visitor to database.
 * Send a POST request to localhost:3000/api/visitors with body
 * {
-* 	"name": "Bob"
+*   "name": "Bob"
 * }
 */
 app.post("/api/visitors", function (request, response) {
@@ -25,16 +89,7 @@ app.post("/api/visitors", function (request, response) {
     response.send(doc);
     return;
   }
-  // insert the username as a document
-  mydb.insert(doc, function(err, body, header) {
-    if (err) {
-      console.log('[mydb.insert] ', err.message);
-      response.send("Error");
-      return;
-    }
-    doc._id = body.id;
-    response.send(doc);
-  });
+  insertOne[vendor](doc, response);
 });
 
 /**
@@ -54,16 +109,7 @@ app.get("/api/visitors", function (request, response) {
     response.json(names);
     return;
   }
-
-  mydb.list({ include_docs: true }, function(err, body) {
-    if (!err) {
-      body.rows.forEach(function(row) {
-        if(row.doc.name)
-          names.push(row.doc.name);
-      });
-      response.json(names);
-    }
-  });
+  getAll[vendor](response);
 });
 
 
@@ -78,7 +124,39 @@ const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
 
 const appEnv = cfenv.getAppEnv(appEnvOpts);
 
-if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
+
+if (appEnv.services['compose-for-mongodb'] || appEnv.getService(/.*[Mm][Oo][Nn][Gg][Oo].*/)) {
+  // Load the MongoDB library.
+  var MongoClient = require('mongodb').MongoClient;
+
+  dbName = 'mydb';
+
+  // Initialize database with credentials
+  if (appEnv.services['compose-for-mongodb']) {
+    MongoClient.connect(appEnv.services['compose-for-mongodb'][0].credentials.uri, null, function(err, db) {
+      if (err) {
+        console.log(err);
+      } else {
+        mydb = db.db(dbName);
+        console.log("Created database: " + dbName);
+      }
+    });
+  } else {
+    // user-provided service with 'mongodb' in its name
+    MongoClient.connect(appEnv.getService(/.*[Mm][Oo][Nn][Gg][Oo].*/).credentials.uri, null,
+      function(err, db) {
+        if (err) {
+          console.log(err);
+        } else {
+          mydb = db.db(dbName);
+          console.log("Created database: " + dbName);
+        }
+      }
+    );
+  }
+
+  vendor = 'mongodb';
+} else if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/[Cc][Ll][Oo][Uu][Dd][Aa][Nn][Tt]/)) {
   // Load the Cloudant library.
   var Cloudant = require('cloudant');
 
@@ -92,7 +170,7 @@ if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
   }
 
   //database name
-  var dbName = 'mydb';
+  dbName = 'mydb';
 
   // Create a new "mydb" database.
   cloudant.db.create(dbName, function(err, data) {
@@ -102,6 +180,8 @@ if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/cloudant/)) {
 
   // Specify the database we are going to use (mydb)...
   mydb = cloudant.db.use(dbName);
+
+  vendor = 'cloudant';
 }
 
 //serve static file (index.html, images, css)
