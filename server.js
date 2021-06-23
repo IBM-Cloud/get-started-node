ba1d01a39daed70daf046a365e93b64b154648ed
PUSHPAGHJH <67584747+PUSHPAGHJH@users.noreply.github.com>
2021-06-23 10:59:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -196,7 +196,7 @@ if(cloudant) {
 //serve static file (index.html, images, css)
 app.use(express.static(__dirname + '/views'));
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3001
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
